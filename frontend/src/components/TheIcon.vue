628161c1acdd199baae3e8a38fd7b22a252eae3f
HanJing1027 <71305380+HanJing1027@users.noreply.github.com>
2025-06-11 21:17:49 +0800

    feat: 新增 TheIcon 圖標組件
    1. 新增可重用的 SVG 圖標組件
    2. 支援透過 icon 屬性指定想要顯示的圖標

---
+++ frontend/src/components/TheIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg>
+    <use :href="`${sprite}#icon-${icon}`" />
+  </svg>
+</template>
+
+<script setup>
+import sprite from '@/assets/icons/sprite.svg'
+
+const props = defineProps({
+  icon: {
+    type: String,
+  },
+})
+</script>
+
+<style lang="scss" setup></style>
