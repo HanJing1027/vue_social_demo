ffb47860bc4c44969aad8726671aaf5781ea43ea
HanJing1027 <71305380+HanJing1027@users.noreply.github.com>
2025-06-12 15:23:04 +0800

    refactor: 將首頁和搜尋頁面路由移至需要驗證的路由群組
    1. 將 home 和 search_result 路由從 publicRoutes 移至 authRoutes
    2. 確保過後導航守衛，用戶需要登入才能訪問主要功能頁面

--- frontend/src/router/modules/authRoutes.js
+++ frontend/src/router/modules/authRoutes.js
@@ -1,5 +1,15 @@
 export default [
   {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomePage.vue'),
+  },
+  {
+    path: '/search_result',
+    name: 'search_result',
+    component: () => import('@/views/SearchPage.vue'),
+  },
+  {
     path: '/profile',
     name: 'profile',
     component: () => import('@/views/ProfilePage.vue'),
