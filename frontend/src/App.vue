22780da6294a8aa23ceb24a32cea40ad8acec865
HanJing1027 <71305380+HanJing1027@users.noreply.github.com>
2025-07-09 01:10:14 +0800

    refactor: 重構全域模態視窗管理，新增 GlobalModals 組件
    1. 將 App.vue 中的全域模態視窗抽離至 GlobalModals.vue
    2. 新增 components/global 資料夾存放全域組件
    3. 提升 App.vue 可讀性和組織結構

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,25 +19,14 @@
     </footer>
   </template>
 
-  <!-- 貼文上傳彈跳視窗 -->
-  <PostUpload v-if="modalStore.modals.postUpload" />
-  <PostDetails v-if="modalStore.modals.postDetails" />
-  <TheConfirmDeleteModal />
-  <TheToast />
+  <GlobalModals />
 </template>
 
 <script setup>
-import TheLayout from './components/layout/TheLayout.vue'
+import TheLayout from '@/components/layout/TheLayout.vue'
 import NavBar from '@/components/layout/NavBar.vue'
-import Footer from './components/layout/Footer.vue'
-import PostUpload from '@/components/post/PostUpload.vue'
-import PostDetails from '@/components/post/PostDetails.vue'
-import TheToast from '@/components/common/TheToast.vue'
-import TheConfirmDeleteModal from '@/components/common/TheConfirmDeleteModal.vue'
-
-import { useModalStore } from '@/stores/modules/modalStore'
-
-const modalStore = useModalStore()
+import Footer from '@/components/layout/Footer.vue'
+import GlobalModals from '@/components/global/GlobalModals.vue'
 </script>
 
 <style lang="scss" scoped></style>
---
+++ frontend/src/components/global/GlobalModals.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="global-modals">
+    <!-- 貼文相關模態視窗 -->
+    <PostUpload v-if="modalStore.modals.postUpload" />
+    <PostDetails v-if="modalStore.modals.postDetails" />
+
+    <!-- 通用模態視窗 -->
+    <TheConfirmDeleteModal />
+
+    <!-- 全域通知 -->
+    <TheToast />
+  </div>
+</template>
+
+<script setup>
+import PostUpload from '@/components/post/PostUpload.vue'
+import PostDetails from '@/components/post/PostDetails.vue'
+import TheToast from '@/components/common/TheToast.vue'
+import TheConfirmDeleteModal from '@/components/common/TheConfirmDeleteModal.vue'
+
+import { useModalStore } from '@/stores/modules/modalStore'
+
+const modalStore = useModalStore()
+</script>
+
+<style lang="scss" scoped></style>
